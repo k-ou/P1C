c09eaba76e8fa64eee287033c6ce70077fbbf0f4
k-ou <kellio94@ucla.edu>
2015-10-27 04:37:04 -0700

    FIXED PAGE NAVIGATION

--- query.php
+++ query.php
@@ -52,7 +52,7 @@ font-family: Garamond, Arial, serif;
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">IMDB</a>
+      <a class="navbar-brand" href="./query.php">IMDB</a>
    </div>
    <!-- Collect the nav links, forms, and other content for toggling -->
    <div class="collapse navbar-collapse">
@@ -61,10 +61,10 @@ font-family: Garamond, Arial, serif;
       <li class="dropdown active">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Add New Content <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li role="presentation"><a href=".addActorDir" role="tab" data-toggle="tab">Add Actor/Director</a></li>
-          <li role="presentation"><a href=".addMovie" role="tab" data-toggle="tab">Add Movie Information</a></li>
-          <li role="presentation"><a href=".addMovActRel" role="tab" data-toggle="tab">Add Movie/Actor Relation</a></li>
-          <li role="presentation"><a href=".addMovDirRel" role="tab" data-toggle="tab">Add Movie/Director Relation</a></li>
+          <li><a href="./addActorDir.php">Add Actor/Director</a></li>
+          <li><a href="./addMovie.php">Add Movie Information</a></li>
+          <li><a href="./addMovActRel.php">Add Movie/Actor Relation</a></li>
+          <li><a href="./addMovDirRel.php">Add Movie/Director Relation</a></li>
           <li role="separator" class="divider"></li>
           <li><a href="#">Separated link</a></li>
         </ul>
@@ -74,8 +74,8 @@ font-family: Garamond, Arial, serif;
       <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Browse<span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li role="presentation"><a href=".actorInfo" role="tab" data-toggle="tab">All Actors/Actresses</a></li>
-          <li role="presentation"><a href=".movieInfo" role="tab" data-toggle="tab">All Movies</a></li>
+          <li><a href="./actorInfo.php">All Actors/Actresses</a></li>
+          <li><a href="./movieInfo.php">All Movies</a></li>
         </ul>
       </li>
   <!--end dropdown-->
@@ -88,7 +88,7 @@ font-family: Garamond, Arial, serif;
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-        <li role="presentation"><a href=".movieDBQuery" role="tab" data-toggle="tab">Enter Query</a></li>
+        <li><a href="./movieDBQuery.php">Enter Query</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
           <ul class="dropdown-menu">
@@ -112,206 +112,13 @@ font-family: Garamond, Arial, serif;
 
 <div class="col-md-6 tab-content">
 
-<!--HOME-->
-<div class="home tab-pane active" role="tabpanel">
+  <!--HOME-->
+<div class="home">
 
 <h1>Welcome to IMDB.</h1>
 
 </div>
 <!--END HOME-->
-
-<!--MOVIE DB-->
-
-<div class="movieDBQuery tab-pane" role="tabpanel">
-
-  <h1>Movie Database Query</h1>
-  <p>(Ver 1.0 10/26/2015 by Sharon Grewal and Kelly Ou)<br>
-  Type a query in the following box, please use correct SELECT statements. <br>
-  For example, type in SELECT * FROM Actor WHERE id=10; </p>
-
-<!--form action="<?php $_SERVER['PHP_SELF'];?>" method="get"-->
-<form action="movieDBQuery.php" method="get">
-<textarea rows="4" cols"50" name="query"></textarea>
-<br>
-<input type="submit" name="submit" value="submit query">
-</form>
-
-
-<!--PHP for Movie Data Query-->
-
-<?php include 'movieDBQuery.php';?>
-
-
-</div> 
-
-<!--END MOVIE DB-->
-
-<!--ADD ACTOR / DIR-->
-<div class="addActorDir tab-pane" role="tabpanel">
-
-  <h1>Add Actor / Director</h1>
-  <p>(Ver 1.0 10/26/2015 by Sharon Grewal and Kelly Ou)<br>
-  Add an actor/actress or a director.</p>
-
-<form action="<?php $_SERVER['PHP_SELF'];?>" method="get">
-  <input type="radio" name="position" value="Actor" checked>Actor/Actress
-  <input type="radio" name="position" value="Director">Director
-  <br>
-  First name:<br>
-  <input type="text" name="firstName">
-  <br>
-  Last name:<br>
-  <input type="text" name="lastName">
-  <br>
-  Sex:
-  <input type="radio" name="sex" value="female" checked>Female
-  <input type="radio" name="sex" value="male">Male
-  <br>
-  Date of Birth (yyyy-mm-dd):<br>
-  <input type="text" name="dob">
-  <br>
-  Date of Death (yyyy-mm-dd or leave blank):<br>
-  <input type="text" name="dod">
-  <br><br>
-  <input type="submit" value="Submit">
-</form>
-
-<<<<<<< HEAD
-<?php
-$db_connection = mysql_connect("localhost", "cs143", "");
-if(!$db_connection){
-   $errmsg = mysql_error($db_connection);
-   print "Connection failed: $errmsg <br />";
-   exit(1);
-}
-mysql_select_db("TEST", $db_connection);
-
-$position = "";
-$firstName = "";
-$lastName = "";
-$sex = "";
-$dob = "";
-$dod = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-   $position = $_GET["position"];
-   $firstName = $_GET["firstName"];
-   $lastName = $_GET["lastName"];
-   $sex = $_GET["sex"];
-   $dob = $_GET["dob"];
-   $dod = $_GET["dod"]; 
-}
-
-print "position: " . $position . "<br>firstName: " . $firstName;
-
-$updateID = mysql_query("UPDATE MaxPersonID SET id=id+1", $db_connection);
-$query = "INSERT INTO '$position' VALUES ('$updateID', '$firstName', '$lastName', '$sex', '$dob', '$dod')";
-$result = mysql_query($query, $db_connection);
-
-//must check for violations for insertion/deletion/updates
-//currently
-if ($_GET["submit"]) {
-   if (!$result) {
-   $message  = 'Invalid query: ' . mysql_error() . "\n";
-   $message .= 'Whole query: ' . $query;
-   die($message);
-   }
-   else {
-   print "You've successfully added <br />" . $firstName . " " . $lastName;
-   }
-}
-
-mysql_free_result($result);
-mysql_close($db_connection);
-
-?>
-=======
-<?php include 'addActorDir.php';?>
->>>>>>> refs/remotes/origin/master
-
-</div>
-<!--END ADD ACTOR / DIR-->
-
-<!--ADD MOVIE-->
-<div class="addMovie tab-pane" role="tabpanel">
-
-  <h1>Add Movie</h1>
-  <p>(Ver 1.0 10/26/2015 by Sharon Grewal and Kelly Ou)<br>
-  <p>Add New Movie:</p>
-
-<form action="<?php $_SERVER['PHP_SELF'];?>" method="get">
-  Title:<br>
-  <input type="text" name="title">
-  <br>
-  Company:<br>
-  <input type="text" name="company"><br>
-    Year:<br>
-  <input type="text" name="year"><br>
-    Genre:<br>
-  <input type="checkbox" name="action">Action<br>
-    <input type="checkbox" name="adult">Adult<br>
-    <input type="checkbox" name="adventure">Adventure<br>
-    <input type="checkbox" name="animation">Animation<br>
-    <input type="checkbox" name="comedy">Comedy<br>
-    <input type="checkbox" name="crime">Crime<br>
-    <input type="checkbox" name="documentary">Documentary<br>
-    <input type="checkbox" name="family">Family<br>
-    <input type="checkbox" name="fantasy">Fantasy<br>
-    <input type="checkbox" name="horror">Horror<br>
-    <input type="checkbox" name="musical">Musical<br>
-    <input type="checkbox" name="myster">Mystery<br>
-    <input type="checkbox" name="romance">Romance<br>
-    <input type="checkbox" name="sci-fi">Sci-Fi<br>
-    <input type="checkbox" name="short">Short<br>
-    <input type="checkbox" name="thriller">Thriller<br>
-    <input type="checkbox" name="war">War<br>
-    <input type="checkbox" name="western">Western<br>
-  <br><br>
-  <input type="submit" value="Submit">
-</form>
-
-</div>
-<!--END ADD MOVIE-->
-
-<!--START ADD MOVIE / ACTOR RELATION-->
-<div class="addMovActRel tab-pane" role="tabpanel">
-
-  <h1>Add Movie / Actor Relation</h1>
-  <p>(Ver 1.0 10/26/2015 by Sharon Grewal and Kelly Ou)<br>
-  Input a movie and the actor/actress that took part, as well as their role in the movie.</p>
-
-</div>
-<!--END ADD MOVIE / ACTOR RELATION-->
-
-<!--START ADD MOVIE / DIRECTOR RELATION-->
-<div class="addMovDirRel tab-pane" role="tabpanel">
-
-  <h1>Add Movie / Director Relation</h1>
-  <p>(Ver 1.0 10/26/2015 by Sharon Grewal and Kelly Ou)<br>
-  Input a movie and the director that directed it.</p>
-
-</div>
-<!--END ADD MOVIE / ACTOR RELATION-->
-
-<!--START ACTOR INFO-->
-<div class="actorInfo tab-pane" role="tabpanel">
-
-  <h1>Actor Info</h1>
-  <p>(Ver 1.0 10/26/2015 by Sharon Grewal and Kelly Ou)<br>
-  Select an actor or actress.</p>
-
-</div>
-<!--END ADD MOVIE / ACTOR RELATION-->
-
-<!--START ADD MOVIE / DIRECTOR RELATION-->
-<div class="movieInfo tab-pane" role="tabpanel">
-
-  <h1>Movie Info</h1>
-  <p>(Ver 1.0 10/26/2015 by Sharon Grewal and Kelly Ou)<br>
-  Select a movie.</p>
-
-</div>
-<!--END ADD MOVIE / ACTOR RELATION-->
 
 </div>
 
